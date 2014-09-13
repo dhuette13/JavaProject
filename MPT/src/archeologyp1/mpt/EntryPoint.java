ce0bb2470d062868579d02a5dc935c99dbecd8d0
Daniel Huette <dhuette13@yahoo.com>
2014-09-13 11:14:04 -0500

    Fixed indent

--- MPT/src/archeologyp1/mpt/EntryPoint.java
+++ MPT/src/archeologyp1/mpt/EntryPoint.java
@@ -20,54 +20,54 @@ public class EntryPoint {
 	String path;
 	Map map;
 	int row, col, width, height;
-	
+
 	public EntryPoint(){
 		input = new Scanner(System.in);
 		flag = true;
 	}
 
 	public void go(){
-			do{
-				try{
-					System.out.println("1 ) Load Map ");
-					System.out.println("2 ) Generate Map ");
-					System.out.print("::> ");
-					selection = input.nextInt();
-					switch(selection){ 
-						case 1:
-							System.out.print("Enter path name: ");
-							path = input.next();
-							//File f = new File(path);
-							//if(f.exists() == false)
-							//{
-							//   throw new FileNotFoundException("This file cannot be found. Please try again.");
-							//}
-							//probably need an else-if and an else in here too, need to think more on what they'd do though
-							map = Utilities.load(path);
-							if(map != null)
-								flag = false;
-							break;
-						case 2:
-							System.out.print("Enter the desired dimensions (width height): " );
-							// Check format of the user's input? (width height) or (width, height) 
-							width = input.nextInt();
-							height = input.nextInt();
-							map = Utilities.generateMap(width, height);
-							if(map != null)
-								flag = false;
-							break;
-						default:
-							System.out.println("Invalid Choice, Try again.");
-					}
-				} catch (InputMismatchException e){
-					System.out.println("Incorrect input, try again.");
-					input.nextLine();
+		do{
+			try{
+				System.out.println("1 ) Load Map ");
+				System.out.println("2 ) Generate Map ");
+				System.out.print("::> ");
+				selection = input.nextInt();
+				switch(selection){ 
+				case 1:
+					System.out.print("Enter path name: ");
+					path = input.next();
+					//File f = new File(path);
+					//if(f.exists() == false)
+					//{
+					//   throw new FileNotFoundException("This file cannot be found. Please try again.");
+					//}
+					//probably need an else-if and an else in here too, need to think more on what they'd do though
+					map = Utilities.load(path);
+					if(map != null)
+						flag = false;
+					break;
+				case 2:
+					System.out.print("Enter the desired dimensions (width height): " );
+					// Check format of the user's input? (width height) or (width, height) 
+					width = input.nextInt();
+					height = input.nextInt();
+					map = Utilities.generateMap(width, height);
+					if(map != null)
+						flag = false;
+					break;
+				default:
+					System.out.println("Invalid Choice, Try again.");
 				}
-			} while(flag);
-			
+			} catch (InputMismatchException e){
+				System.out.println("Incorrect input, try again.");
+				input.nextLine();
+			}
+		} while(flag);
+
 		/* Print map to standard out */
 		Utilities.printMap(map, System.out);
-		
+
 		/* Export map to new file */
 		try{
 			Utilities.printMap(map, new PrintStream(new File("resources/export1.txt")));
@@ -76,15 +76,15 @@ public class EntryPoint {
 			e.printStackTrace();
 		}
 		System.out.println("Goodbye");
-		
-//		flag = true;
-//		while(flag){
-//			System.out.println();
-//			
-//		}
-		
+
+		//		flag = true;
+		//		while(flag){
+		//			System.out.println();
+		//			
+		//		}
+
 	}
-	
+
 	/**
 	 * 
 	 * @param args
