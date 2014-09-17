b0b81772f8c92ec470d927e1707718aa962ab98c
celsor <celine.soriano.1@gmail.com>
2014-09-17 12:29:55 -0500

    Update UserCoordinate.java

--- MPT/src/archeologyp1/mpt/UserCoordinate.java
+++ MPT/src/archeologyp1/mpt/UserCoordinate.java
@@ -1,1 +1,20 @@
-//hello
+package archeologyp1.mpt;
+
+import java.io.File;
+import java.io.PrintStream;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import archeologyp1.shared.Map;
+import archeologyp1.shared.Utilities;
+
+// 1) Change a coordinate
+// a) Change a single coordinate
+// b) Change a row
+// - wizard: feature, viewing symbol, excavated?, find
+// 3) Change a viewing symbol
+// a) Whole map
+// b) Group of symbols
+// 4) Print map
+// 5) Export map
+// 6) Save map
