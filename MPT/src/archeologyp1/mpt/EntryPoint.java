3d4b525410f854999703ee1df5047e29dbe2b04b
Daniel Huette <dhuette13@yahoo.com>
2014-09-23 21:12:17 -0500

    Reorganized MPT

--- MPT/src/archeologyp1/mpt/EntryPoint.java
+++ MPT/src/archeologyp1/mpt/EntryPoint.java
@@ -1,6 +1,7 @@
 package archeologyp1.mpt;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.PrintStream;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -17,33 +18,25 @@ import archeologyp1.shared.Utilities;
 public class EntryPoint {
 	Scanner input;
 	int selection;
-	int mapselect1;
 	int userCoord;
 	int userView;
 	boolean flag;
 	String path;
 	Map map;
 	UserInterface ui;
+	MapEditor mapEditor;
 	int row, col, width, height;
 
 	public EntryPoint(){
-		handleLoad();
 		input = new Scanner(System.in);
+		handleLoad();
+		mapEditor = new MapEditor(map);
+		ui = new UserInterface(mapEditor);
 		flag = true;
 	}
 
 	public void go(){
-		/* Print map to standard out */
-		Utilities.printMap(map, System.out);
-		System.out.println("After print");
-		/* Export map to new file */
-		try{
-			Utilities.printMap(map, new PrintStream(new File("res/export1.txt")));
-			System.out.println("After export");
-		} catch (Exception e){
-			System.err.println("Could not create file");
-			e.printStackTrace();
-		}
+		handleMenu();
 	}
 
 	private void handleLoad(){
@@ -83,68 +76,68 @@ public class EntryPoint {
 	private void handleMenu(){
 		flag = true;
 		while(flag) {
+			Utilities.printMap(map, System.out);
 			System.out.println("Please pick what you would like to do.");
 			System.out.println("1 ) Change a coordinate");
-			System.out.println("2 ) Change a viewing symbol"); //Should we do it so that they have to have more than one symbol before they start changing it?
-			//We'll also have to make sure we keep track of what they use on the map so we know what to prompt them to change.
-			System.out.println("3 ) Print the map");
-			System.out.println("4 ) Export the map");
-			System.out.println("5 ) Save the map");
+			System.out.println("2 ) Change a viewing symbol");
+			System.out.println("3 ) Export the map");
+			System.out.println("4 ) Save the map");
+			System.out.println("0 ) Exit");
 			System.out.println("::> ");
-			mapselect1 = input.nextInt();
-			
-			switch(mapselect1){
-			case 1:
-				//System.out.println("What would you like to change?");
-				//System.out.println("1 ) A single coordinate");
-				//System.out.println("2 ) An entire row");
-				//System.out.println("::> ");
-				//userCoord = input.nextInt();
+			try{
+				selection = input.nextInt();
 				
-				//map = mpt.changeCoordinate(userCoord);
+				switch(selection){
+				/* Edit a coordinate or row of coordinates */
+				case 1:
+					ui.changeCoordinate();
+					break;
+					/* Change the Viewing Options */
+				case 2:                		
+					ui.changeViewing();
+					break;
+					/* Export map */
+				case 3:
+					System.out.print("Enter a path name to export to: ");
+					path = input.next();
+					Utilities.printMap(map, new PrintStream(new File(path)));
+					break;
+					/* Save map */
+				case 4: 
+					break;
+					/* Exit */
+				case 0:
+					System.out.println("Goodbye. ");
+					flag = false;
+					break;
+				default:
+					System.out.println("That is not a valid choice. Please choose again.");
+				}
 				
-				//flag = false;
-				break;
-			case 2:                		
-                //map = mpt.changeViewing(userView);
-                		
-				//flag = false;
-				break;
-			case 3:
-				//flag = false;
-				break;
-			case 4: 
-				//flag = false;
-				break;
-			case 5: 
-				//flag = false;
-				break;
-			default:
-				System.out.println("That is not a valid choice. Please choose again.");
+				// 1) Change a coordinate
+				//  a) Change a single coordinate
+				//  b) Change a row
+				//    - wizard: feature, viewing symbol, excavated?, find 
+				// 3) Change a viewing symbol
+				//  a) Whole map
+				//  b) Group of symbols
+				// 4) Print map
+				// 5) Export map
+				// 6) Save map
+				System.out.println();
+			} catch(InputMismatchException e){
+				System.out.println("Invalid Input");
+			} catch (FileNotFoundException e) {
+				System.out.println("Invalid file name");
 			}
-			
-			// 1) Change a coordinate
-			//  a) Change a single coordinate
-			//  b) Change a row
-			//    - wizard: feature, viewing symbol, excavated?, find 
-			// 3) Change a viewing symbol
-			//  a) Whole map
-			//  b) Group of symbols
-			// 4) Print map
-			// 5) Export map
-			// 6) Save map
-
-			System.out.println();
-
 		}
 	}
+	
 	/**
-	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		EntryPoint entry = new EntryPoint();
 		entry.go();
 	}
-
 }
