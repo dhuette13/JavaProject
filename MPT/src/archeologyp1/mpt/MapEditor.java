946f476a5aab80c5b0b68a313df0e5afb173cbf5
celsor <celine.soriano.1@gmail.com>
2014-09-24 17:07:29 -0500

    got rid of unnecessary lines

--- MPT/src/archeologyp1/mpt/MapEditor.java
+++ MPT/src/archeologyp1/mpt/MapEditor.java
@@ -1,6 +1,6 @@
 package archeologyp1.mpt;
 
-import java.util.Scanner;
+import java.util.Scanner; 
 
 import archeologyp1.shared.Coordinate;
 import archeologyp1.shared.Map;
@@ -16,23 +16,24 @@ public class MapEditor {
 		this.map = map;
 	}
 	
-	//how to parameter
-	public void editSingleCoordinate(int row, char col, int type, int amount, char featureChange){
+	public void changeFeature(){
+		//current.setFeature(char)
+	}
+	
+	public void changeDate(int row, char col, int type, int amount){
 		Scanner input = new Scanner(System.in);
 		int date = 0;
 		int r = row - 1;
 		int c = Utilities.columnToIndex(Character.toString(col)); 
-		current = map.plane[row][col];
+		current = map.plane[r][c];
 		
 		//Inputting the dates
-		for(int i=0; i<amount-1; i++){
+		for(int i=0; i<amount; i++){
 			System.out.println("What date would you like your " + (i+1) + " find to have?");
 			System.out.println("::> ");
 			date = input.nextInt();
 			current.potCount.add(new Pot(date));
 		}
-		
-		//current.setFeature(char)
 	}
 
 	public void editRow(int row){
@@ -40,14 +41,5 @@ public class MapEditor {
 		for(int c = 0; c < map.getNumColumns(); c++){
 			current = map.plane[row][c];
 		}
-	}
-	
-	//?!?!?!?!?!
-	public void editDates(){
-		
-	}
-	
-	public void editFeatures(){
-		
 	}
 }
