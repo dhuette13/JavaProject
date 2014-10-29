a92f9b759e5b2202317c6e87222ee431cfb6d06e
celsor <celine.soriano.1@gmail.com>
2014-10-28 20:28:44 -0500

    made the class for the report

--- ADT/src/archeologyp2/adt/Report.java
+++ ADT/src/archeologyp2/adt/Report.java
@@ -1,10 +1,25 @@
 package archeologyp2.adt;
 
-/* Layout for Report
+/* Temporary Layout for Report
 
+ -------------------------------------------------------------------
  ||   TYPE   ||   ROW   ||   COLUMN   ||   DATE   ||   PROPERTY   ||
+ -------------------------------------------------------------------
+ ||          ||         ||            ||          ||              ||
+ -------------------------------------------------------------------
+ Average Date: -----
  
   */
+
+/**
+ * REPORT FOR THE ADT
+ * 
+ * This class specifies on working on the report and
+ * printing it out to the user. 
+ * 
+ * @author Daniel
+ * @author Celine
+ */
 
 public class Report {
 
