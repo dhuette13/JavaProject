707310622a41ebeef4ae063063ad021a536ccd8d
celsor <celine.soriano.1@gmail.com>
2014-11-03 18:12:15 -0600

    javadoc for baseconvert

--- Shared/src/archeologyp2/shared/gui/BaseConverterDialog.java
+++ Shared/src/archeologyp2/shared/gui/BaseConverterDialog.java
@@ -22,6 +22,17 @@ import javax.swing.JTextField;
 import archeologyp2.shared.map.Utilities;
 
 /**
+ * 
+ * BASE CONVERTER FOR SHARED GUI
+ * 
+ * This class contains the graphical interface of the
+ * base converter the user is allowed to use to 
+ * calculate indexes to columns, and columns to
+ * indexes. When the user types into one of the two
+ * given fields and click okay, the other text area
+ * will fill up with what it's converted to. 
+ * 
+ * @author Daniel
  * @author Celine
  *
  */
@@ -39,6 +50,16 @@ public class BaseConverterDialog extends JDialog implements ActionListener, KeyL
 	private JButton cancelButton;
 
 
+	/**
+	 * For public BaseConverterDialog
+	 * This contains a GridBagLayout, which holds two
+	 * labels, two text fields, and two buttons, both
+	 * of which the user can click on to get the 
+	 * results they need. After entering what the user
+	 * needs to be converted, the user will click "OK" 
+	 * and see what it's converted to. When they're done,
+	 * they can just click on the "EXIT" button to get out. 
+	 */
 	public BaseConverterDialog(){
 		setVisible(true);
 		setTitle("Base 26 Tool");
@@ -110,6 +131,12 @@ public class BaseConverterDialog extends JDialog implements ActionListener, KeyL
 
 
 	@Override
+	/**
+	 * For public void actionPerformed
+	 * This method launches the logic and grabs
+	 * the data from where it resides in the Utilities
+	 * class of the Shared Folder. 
+	 */
 	public void actionPerformed(ActionEvent arg0) {
 		int index;
 		String col;
@@ -127,6 +154,11 @@ public class BaseConverterDialog extends JDialog implements ActionListener, KeyL
 	}
 
 	@Override
+	/**
+	 * For public void keyTyped 
+	 * Allows the user to hit their ENTER bar
+	 * for "OK"
+	 */
 	public void keyTyped(KeyEvent e) {
 		if(e.getKeyCode() == KeyEvent.VK_ENTER){
 			confirmButton.doClick();
@@ -134,20 +166,32 @@ public class BaseConverterDialog extends JDialog implements ActionListener, KeyL
 	}
 	
 	@Override
+	/**
+	 * For public void keyPressed
+	 */
 	public void keyPressed(KeyEvent arg0) {
 	}
 	
 	@Override
+	/**
+	 * For public void keyReleased
+	 */
 	public void keyReleased(KeyEvent arg0) {
 	}
 	
 	@Override
+	/**
+	 * For public void focusGained
+	 */
 	public void focusGained(FocusEvent e) {
 		indexArea.setText("");
 		colArea.setText("");
 	}
 
 	@Override
+	/**
+	 * For public void focusLost
+	 */
 	public void focusLost(FocusEvent arg0) {
 	}
 }
